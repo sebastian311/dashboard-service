92e8c4bd8adfc057f4cfece31d31dbb5d668d664
Sebastian <naesebastian96@gmail.com>
2026-04-29 19:24:53 +0300

    created JWT service

---
+++ src/main/java/com/dashboard/dashboardxservice/services/JwtUtil.java
@@ -0,0 +1,28 @@
+package com.dashboard.dashboardxservice.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(String username) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
