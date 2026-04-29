05f7b330344e4a77b33769c81ec730ce41c8c0b6
Sebastian <naesebastian96@gmail.com>
2026-04-29 19:07:23 +0300

    project initialized

---
+++ src/main/java/com/dashboard/dashboardxservice/DashboardxserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboardxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardxserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardxserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboard/dashboardxservice/DashboardxserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboardxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardxserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
